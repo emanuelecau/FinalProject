2f5ea185f28003e3c11369ac82c5ca9b12b511e6
emanuelecau <121568758+emanuelecau@users.noreply.github.com>
2023-07-13 18:46:57 +0200

    secondocommit

--- cocoFront/src/pages/NewCourse.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export function NewCourse() {
-    return (
-        <h1>NewCourse</h1>
-    )
-}
--- cocoFront/src/pages/NotFound.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export function NotFound() {
-    return (
-        <h1>Page not Found :' </h1>
-    )
-}
